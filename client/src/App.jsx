76104aa7f260647c96f729b72b07b3362b7a0cab
Mdletshe <s.mdletshe@marval.co.uk>
2023-12-01 22:16:36 +0200

    added header, sign in and up page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,25 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import logo from './assets/logo_transparent.png'
+import IndexPage from './pages/IndexPage'
+import SignInPage from './pages/SignInPage'
+import RegisterPage from './pages/SignUpPage'
+import Layout from './Layout'
 
 function App() {
 
   return (
-    <div className="bg-red-500">Cozy Haven </div>
-      )
+    // <div className='p-4 flex justify-between'>
+    // <img src={logo} alt="Logo" className='w-20 h-20'/>
+
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path='/signin' element={<SignInPage />} />
+        <Route path='/signup' element={<RegisterPage />} />
+      </Route>
+    </Routes>
+  )
 }
 
 export default App
