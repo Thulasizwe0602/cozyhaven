5d76835c3e90a31110528117ac81602bf1500aa8
Mdletshe <s.mdletshe@marval.co.uk>
2023-12-01 22:19:19 +0200

    added name input

--- client/src/pages/SignUpPage.jsx
+++ client/src/pages/SignUpPage.jsx
@@ -5,11 +5,12 @@ export default function SignUpPage() {
             <div className="mb-80">
             <h1 className="text-4xl text-center mb-4 font-bold">Sign Up</h1>
             <form className="max-w-md mx-auto ">
+                <input type='text' placeholder='Name'/>
                 <input type='email' placeholder='Email address'/>
                 <input type='password' placeholder='Password'/>
                 <input type='password' placeholder='Confirm Password'/>
                 <button className="primary">Sign Up</button>
-                <div className="text-center py-2 text-gray-500">Got account?   <Link className='underline text-primary'to={'/signin'}>Sign In</Link>
+                <div className="text-center py-2 text-gray-500">Already a member?   <Link className='underline text-primary'to={'/signin'}>Sign In</Link>
                 </div>
             </form>
             </div>
