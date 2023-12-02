cc1cc0c4e901f075b96c1260889c9cb29c7eb59b
Mdletshe <s.mdletshe@marval.co.uk>
2023-12-02 16:42:03 +0200

    interim

--- client/src/pages/SignInPage.jsx
+++ client/src/pages/SignInPage.jsx
@@ -1,32 +1,51 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
-export default function SignInPage() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+import { useState } from 'react';
+import axios from "axios";
 
-    async function handleSignInUser(event) {
+export default function SignInPage() {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({ ...prevData, [name]: value }));
+    };
+
+    const handleSignInUser = async (event) => {
         event.preventDefault();
         try {
-            await axios.post('/signIn', {
-                email,
-                password
-            });
-        } catch (e) {
-            alert(e.response.data.errorMessage)
+            await axios.post('/signin', formData);
+        } catch (error) {
+            alert(error);
         }
-    }
+    };
+
     return (
         <div className="mt-4 grow flex items-center justify-around">
             <div className="mb-80">
                 <h1 className="text-4xl text-center mb-4 font-bold">Sign In</h1>
                 <form className="max-w-md mx-auto " onSubmit={handleSignInUser}>
-                    <input type='email' placeholder='Email address' value={email} onChange={event => setEmail(event.target.value)} />
-                    <input type='password' placeholder='Password' value={password} onChange={event => setPassword(event.target.value)} />
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Email address"
+                        value={formData.email}
+                        onChange={handleChange}
+                    />
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        value={formData.password}
+                        onChange={handleChange}
+                    />
                     <button className="primary">Sign In</button>
                     <div className="text-center py-2 text-gray-500">Don't have an account?   <Link className='underline text-primary' to={'/signup'}>Sign Up</Link>
                     </div>
                 </form>
             </div>
         </div>
-    )
+    );
 }
--- client/src/pages/SignUpPage.jsx
+++ client/src/pages/SignUpPage.jsx
@@ -1,39 +1,72 @@
 import { Link } from "react-router-dom";
-import {useState} from 'react';
+import { useState } from 'react';
 import axios from "axios";
+
 export default function SignUpPage() {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+    });
 
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({ ...prevData, [name]: value }));
+    };
 
-    async function handleSignUpUser(event){
+    const handleSignUpUser = async (event) => {
         event.preventDefault();
-        try{
-            await axios.post('/signup',{
-                name,
-                email,
-                password
-            });
-        }catch(e){
-            alert(e.response.data.errorMessage)
+
+        try {
+            await axios.post('/signup', formData);
+        } catch (error) {
+            alert(error.response.data.errorMessage);
         }
-    }
-    
+    };
+
     return (
         <div className="mt-4 grow flex items-center justify-around">
             <div className="mb-80">
-            <h1 className="text-4xl text-center mb-4 font-bold">Sign Up</h1>
-            <form className="max-w-md mx-auto" onSubmit={handleSignUpUser}>
-                <input type='text' placeholder='Name' value={name} onChange={ev => setName(ev.target.value)}/>
-                <input type='email' placeholder='Email address' value={email} onChange={ev => setEmail(ev.target.value)}/>
-                <input type='password' placeholder='Password' value={password} onChange={ev => setPassword(ev.target.value)}/>
-                <input type='password' placeholder='Confirm Password'/>
-                <button className="primary">Sign Up</button>
-                <div className="text-center py-2 text-gray-500">Already a member?   <Link className='underline text-primary'to={'/signin'}>Sign In</Link>
-                </div>
-            </form>
+                <h1 className="text-4xl text-center mb-4 font-bold">Sign Up</h1>
+                <form className="max-w-md mx-auto" onSubmit={handleSignUpUser}>
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="Name"
+                        value={formData.name}
+                        onChange={handleChange}
+                    />
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Email address"
+                        value={formData.email}
+                        onChange={handleChange}
+                    />
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        value={formData.password}
+                        onChange={handleChange}
+                    />
+                    <input
+                        type="password"
+                        name="confirmPassword"
+                        placeholder="Confirm Password"
+                        value={formData.confirmPassword}
+                        onChange={handleChange}
+                    />
+                    <button className="primary">Sign Up</button>
+                    <div className="text-center py-2 text-gray-500">
+                        Already a member?{' '}
+                        <Link className="underline text-primary" to={'/signin'}>
+                            Sign In
+                        </Link>
+                    </div>
+                </form>
             </div>
         </div>
-    )
+    );
 }
