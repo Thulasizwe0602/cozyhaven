37e6cb5474887787cb875fec49a071e4b74c60c7
Mdletshe <s.mdletshe@marval.co.uk>
2023-12-13 23:59:25 +0200

    added jwt token

--- api/routes/accomodationRoute.js
+++ api/routes/accomodationRoute.js
@@ -1,34 +1,35 @@
 const express = require('express');
-const jwt = require('jsonwebtoken');
+const authenticateToken = require('../middleware/authenticateToken');
 const Accomodation = require('../models/Accomodation.js');
 
 const router = express.Router();
-const jwtSecret = 'cozyhaven';
 
-router.post('/addAccomodation', async (req, res) => {
+router.post('/addAccomodation', authenticateToken, async (req, res) => {
   try {
     const { title, address, description, extraInfo, checkIn, checkOut, maxGuest, photoLink, addedPhotos, indoorFeatures, outdoorFeatures } = req.body;
-    const { token } = req.cookies;
-    jwt.verify(token, jwtSecret, {}, async (error, cookieUserData) => {
-      if (error) {
-        throw error;
-      }
-      const accomodationDoc = await Accomodation.create({
-        owner: cookieUserData.id,
-        title,
-        address,
-        description,
-        extraInfo,
-        checkIn,
-        checkOut,
-        maxGuest,
-        photoLink,
-        photos: addedPhotos,
-        indoorFeatures,
-        outdoorFeatures
-      });
-      res.status(201).json({ data: accomodationDoc, message: 'Accommodation created successfully' });
+    const userData = req.userData;
+
+    if (!userData) {
+      res.status(401).json({ message: 'Unauthorized' });
+      return;
+    }
+
+    const accomodationDoc = await Accomodation.create({
+      owner: userData.id,
+      title,
+      address,
+      description,
+      extraInfo,
+      checkIn,
+      checkOut,
+      maxGuest,
+      photoLink,
+      photos: addedPhotos,
+      indoorFeatures,
+      outdoorFeatures
     });
+
+    res.status(201).json({ data: accomodationDoc, message: 'Accommodation created successfully' });
   } catch (error) {
     console.error('Error during accommodation creation:', error);
     res.status(500).json({ errorTitle: 'Internal Server Error', errorMessage: error });
--- api/routes/profileRoute.js
+++ api/routes/profileRoute.js
@@ -1,23 +1,19 @@
 const express = require('express');
-const jwt = require('jsonwebtoken');
-const User = require('../models/User.js');
-
+const jwtMiddleware = require('../middleware/authenticateToken');
 const router = express.Router();
-const jwtSecret = 'cozyhaven';
 
-router.get('/', (req, res) => {
-    const { token } = req.cookies;
-    if (token) {
-      jwt.verify(token, jwtSecret, {}, async (error, cookieUserData) => {
-        if (error) {
-          throw error;
-        }
-        const { id, name, email } = await User.findById(cookieUserData.id);
-        res.json({ id, name, email });
-      });
-    } else {
-      res.json('null');
-    }
+router.get('/', jwtMiddleware, (req, res) => {
+  const userData = req.userData;
+
+  if (userData) {
+    res.json({
+      id: userData.id,
+      name: userData.name,
+      email: userData.email
+    });
+  } else {
+    res.json('null');
+  }
 });
 
 module.exports = router;
